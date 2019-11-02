7cce01aea774db7014ae4698611dfb650d0fc029
Aaron Lewis <aaronlewis54321@gmail.com>
2019-11-02 02:42:46 -0400

    Init take 47

---
+++ mycharitable/src/main/java/com/application/mycharitable/MycharitableApplication.java
@@ -0,0 +1,13 @@
+package com.application.mycharitable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycharitableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycharitableApplication.class, args);
+	}
+
+}
