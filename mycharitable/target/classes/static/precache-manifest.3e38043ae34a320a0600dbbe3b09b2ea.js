b6df6e264e692e5e90ed8dfae147b063d0af516c
Naymish Vaghela <vnaymish@gmail.com>
2019-11-02 03:33:05 -0400

    sending target folder

---
+++ mycharitable/target/classes/static/precache-manifest.3e38043ae34a320a0600dbbe3b09b2ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee62f99333699560622fc228c45e9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1caf72e9f3defc8986bb",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "97d85c168dc2e420ae5c",
+    "url": "/static/js/2.6346d688.chunk.js"
+  },
+  {
+    "revision": "1caf72e9f3defc8986bb",
+    "url": "/static/js/main.cc54e98a.chunk.js"
+  },
+  {
+    "revision": "44a2153cd947f43b4e4c",
+    "url": "/static/js/runtime-main.c7d0873f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
